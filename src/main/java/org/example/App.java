581d277b2ce419dc768b36921dee14a9f3adcaeb
Li Zhanhui <lizhanhui@gmail.com>
2023-06-09 10:11:25 +0800

    Run and gather statistics every second
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,29 +1,39 @@
 package org.example;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
     static {
         System.loadLibrary("native");
     }
 
-    public static void main( String[] args ) throws Exception
-    {
-        AtomicLong total = new AtomicLong(0);
-        AtomicInteger count = new AtomicInteger(0);
+    public static void main(String[] args) throws Exception {
+        final AtomicLong total = new AtomicLong(0);
+        final AtomicInteger count = new AtomicInteger(0);
         App app = new App();
         System.out.println("Start to call native method");
-        for (int i = 0; i < count; i++) {
+
+        Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+            public void run() {
+                long t = total.getAndSet(0);
+                long c = count.getAndSet(0);
+                if (c > 0) {
+                    System.out.println("AVG: " + (t / c) + "ns");
+                }
+            }
+        }, 1, TimeUnit.SECONDS);
+
+        while (true) {
             long start = System.nanoTime();
             app.foo();
-            total += System.nanoTime() - start;
-        }
-        System.out.println("AVG: " + total / count + "ns");
-        while (true) {
-            Thread.sleep(1000);
+            count.incrementAndGet();
+            total.addAndGet(System.nanoTime() - start);
         }
     }
 
